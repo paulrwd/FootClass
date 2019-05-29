6c57c1663dc7df9a3735f085e898bb7902228f03
paulrwd85@gmail.com <paulrwd85@gmail.com>
2019-05-29 11:17:02 +0300

    tcp server 2905

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,6 +8,8 @@ using System.Security.Policy;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 using FootClass;
 
 namespace Calculator
@@ -22,103 +24,38 @@ namespace Calculator
 
 		static void Main(string[] args)
 		{
-			#region thread
+			const string ip = "127.0.0.1";
+			const int port = 8080;
+			var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
 
-
-
-
-			//Thread thread1 = new Thread(new ThreadStart(DoWork1));
-			//thread1.Start();
-
-			//Thread  thread2 = new Thread(new ParameterizedThreadStart(DoWork2));
-			//thread2.Start(int.MaxValue);
-
-			//int j = 0;
-			//for (int i = 0; i < int.MaxValue; i++)
-			//{
-			//	j++;
-
-			//	if (j % 10000 == 0)
-			//	{
-			//		Console.WriteLine("DoWork 3");
-			//	}
-			//}
-
-			#endregion
-
-			#region Async/await
-
+			var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 			
+			Console.WriteLine("Введите сообщение");
+			var message = Console.ReadLine();
 
+			var data = Encoding.UTF8.GetBytes(message);
+			tcpSocket.Connect(tcpEndPoint);
+			tcpSocket.Send(data);
 
-			Console.WriteLine("Begin main");
-			DoWork1Async(10);
-			Console.WriteLine("Cont main");
-			for (int i = 0; i < 10; i++)
+			var buffer = new byte[256];
+			var size = 0;
+			var answer = new StringBuilder();
+
+			do
 			{
-				Console.WriteLine("Main");
-				
-			}
-			#endregion
+				size = tcpSocket.Receive(buffer);
+				answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
 
-			var result = SaveFileAsync("test.txt");
-			Console.WriteLine("конец");
+			} while (tcpSocket.Available > 0);
+
+			Console.WriteLine(answer.ToString());
+
+			tcpSocket.Shutdown(SocketShutdown.Both);
+			tcpSocket.Close();
 			Console.ReadLine();
 		}
 
-		static async Task<bool> SaveFileAsync(string path)
-		{
-			var result = await Task.Run(() => SaveFile(path));
-			return result;
-		}
-
-		static bool SaveFile(string path)
-		{
-			var rnd = new Random();
-			string text = "";
-			for (int i = 0; i < 50000; i++)
-			{
-				text += rnd.Next().ToString();
-			}
-
-			using (var sw = new StreamWriter(path, false, Encoding.UTF8))
-			{
-				sw.WriteLine();
-			}
-
-			return true;
-		}
-
-		static async Task DoWork1Async(int max)
-		{
-			await Task.Run(() => DoWork1(max));
-			Console.WriteLine("Do Work Async");
-		}
-
-		static void DoWork1(int max)
-		{
-
-			for (int i = 0; i < max; i++)
-			{
-				Console.WriteLine("DoWork");
-			}
-		}
-
-		static void DoWork2(object max)
-		{
-			int j = 0;
-			for (int i = 0; i < 10; i++)
-			{
-				j++;
-
-				if (j % 1000 == 0)
-				{
-					Console.WriteLine("DoWork 2");
-				}
-			}
-		}
-
-
+		
 		public static int Factorial(int value)
 		{
 			if (value <= 1)
